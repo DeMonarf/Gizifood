5d3973325d3004ae115d75b959b38003ad34ceaa
Monarf <dmonarf@gmail.com>
2021-11-10 20:16:13 +0800

    Push to Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  const APP_ID = 'c358e138';
+  const APP_KEY = 'a2fee1e5376dacaa61cec35e4cad3634';
+
+  const [recipes, setRecipe] = useState([])
+  const [search,setSearch] = useState('')
+  const [query, setQuery] = useState('egg')
+
+  useEffect(async ()=>{
+    getRecipe()
+  },[query])
+
+  const getRecipe = async () =>{
+    const response = await fetch('https://api.edamam.com/api/recipes/v2/?type=public&q='+ query +'&app_id='+ APP_ID +'&app_key='+APP_KEY)
+    const data = await response.json();
+    setRecipe(data.hits)
+    console.log(data.hits)
+  }
+
+  const updateSearch = e =>{
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search)
+    setSearch('')
+  }
+  return(
+    <div className='App'>
+      <div className='Title'>
+      <h1>Gizi&nbsp;</h1><img  src='./1094675.png'/><h1>&nbsp;Food</h1>
+      </div>
+      <p className='Title-Descrip'>Desired Food Recipe</p>
+      <form className='search-form' onSubmit={getSearch}>
+        <input className='search-input' type="text" placeholder='Food or Type Food or Ingredients' value={search} onChange={updateSearch}/>
+        <button className='search-button' type="Submit">Search</button>
+      </form>
+
+      <div className = "recipe">
+        {recipes.map(recipe=>(
+          <Recipe key={recipe.recipe.label}
+                  title={recipe.recipe.label} 
+                  calories={recipe.recipe.calories}
+                  image={recipe.recipe.image}
+                  link={recipe.recipe.url}
+                  ingredients={recipe.recipe.ingredients}
+                  nutrients={recipe.recipe.totalNutrients}/>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import style from './Recipe.module.css';
+// import $ from 'jquery';
+
+const Recipe = ({title,calories,image,link,ingredients,nutrients}) =>{
+    return(
+        <div className ={style.card}>
+            <h1 className={style.title}>{title}</h1>
+            {/* <span className={style.delete} onClick={this.hide_overlay}><p>X</p></span> */}
+            <div className={style.section1}>
+                <div className={style.blok1}>
+                    <img className={style.image} src={image}/>
+                    <a href={link} className={style.instruction}>Instruction</a>
+                </div>
+                <div className={style.blok2}>
+                    <ol className={style.ingredients} type="disc">
+                        <h3>Ingredients</h3>
+                        {ingredients.map(item =>(
+                            <li>{item.text}</li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+            <div className={style.section2}>
+                <button className={style.btn} onClick= {(e)=>{
+                        if(e.target.nextSibling.style.display == 'none'){
+                            e.target.nextSibling.style.display = 'block'
+                        }else{
+                            e.target.nextSibling.style.display = 'none'
+                        }
+                    }}>
+                    Nutrients</button>
+                <table className={style.table}>
+                    <thead>
+                        <tr>
+                            <th>Label</th>
+                            <th>Quantity</th>
+                            <th>Unit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {   Object.keys(nutrients).map(keys=>(
+                                <tr>
+                                    <td className={style.tabletdcol1}>{nutrients[keys].label}</td>    
+                                    <td>{Math.round(nutrients[keys].quantity)}</td>
+                                    <td>{nutrients[keys].unit}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Recipe;
