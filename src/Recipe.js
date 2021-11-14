47c464294ddcbb9b3bd442f247877bf6b7031276
Monarf <dmonarf@gmail.com>
2021-11-14 13:30:19 +0800

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -9,7 +9,7 @@ const Recipe = ({title,calories,image,link,ingredients,nutrients}) =>{
             {/* <span className={style.delete} onClick={this.hide_overlay}><p>X</p></span> */}
             <div className={style.section1}>
                 <div className={style.blok1}>
-                    <img className={style.image} src={image}/>
+                    <img className={style.image} src={image} alt=""/>
                     <a href={link} className={style.instruction}>Instruction</a>
                 </div>
                 <div className={style.blok2}>
@@ -23,7 +23,7 @@ const Recipe = ({title,calories,image,link,ingredients,nutrients}) =>{
             </div>
             <div className={style.section2}>
                 <button className={style.btn} onClick= {(e)=>{
-                        if(e.target.nextSibling.style.display == 'none'){
+                        if(e.target.nextSibling.style.display === 'none'){
                             e.target.nextSibling.style.display = 'block'
                         }else{
                             e.target.nextSibling.style.display = 'none'
